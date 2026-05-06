956caf44db241d9c79871d4bf29470b808ec703f
Kalpanath <kalpanathofficial@gmail.com>
2026-05-06 12:07:29 +0530

    add payment entity

---
+++ src/main/java/com/serenity/entity/Payment.java
@@ -0,0 +1,38 @@
+package com.serenity.entity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+
+@Entity
+@Table(name = "payments")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer paymentId;
+
+    @Column(name = "amount", nullable = false , precision = 10, scale = 2)
+    private BigDecimal amount;
+
+    @Column(name = "payment_date", nullable = false)
+    private LocalDate paymentDate;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    private PaymentStatus status =  PaymentStatus.PENDING;
+
+    @Column(name = "payment_method",length = 50)
+    private String paymentMethod;
+
+}
