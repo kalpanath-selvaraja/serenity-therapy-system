e3cd325c99529929fc05e84a7d94573d67b041bb
Kalpanath <kalpanathofficial@gmail.com>
2026-05-17 16:50:08 +0530

    update TherapistService

--- src/main/java/com/serenity/service/TherapistService.java
+++ src/main/java/com/serenity/service/TherapistService.java
@@ -1,4 +1,36 @@
 package com.serenity.service;
 
+import com.serenity.dao.DAOFactory;
+import com.serenity.dao.TherapistDAO;
+import com.serenity.entity.Therapist;
+
+import java.util.List;
+
 public class TherapistService {
+    // Obtain the DAO through the factory, not by creating it directly
+    private final TherapistDAO therapistDAO = DAOFactory.getTherapistDAO();
+
+    public void addTherapist(Therapist therapist) {
+        therapistDAO.save(therapist);
+    }
+
+    public void updateTherapist(Therapist therapist) {
+        therapistDAO.update(therapist);
+    }
+
+    public void deleteTherapist(Therapist therapist) {
+        therapistDAO.delete(therapist);
+    }
+
+    public Therapist getTherapistById(Long id) {
+        return therapistDAO.getById(Therapist.class, id);
+    }
+
+    public List<Therapist> getAllTherapists() {
+        return therapistDAO.getAll(Therapist.class);
+    }
+
+    public List<Therapist> findBySpecialization(String specialization) {
+        return therapistDAO.findBySpecialization(specialization);
+    }
 }
